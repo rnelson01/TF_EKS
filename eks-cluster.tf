520f93ef25828f9fa6bce8638da919cbfb2bfbe8
Ryan Nelson <44874266+rnelson01@users.noreply.github.com>
2020-12-03 18:22:07 -0600

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -5,7 +5,7 @@ module "eks" {
   subnets         = module.vpc.private_subnets
 
   tags = {
-    Environment = "rn-eks-demo"
+    Environment = "my-eks-demo"
     GithubRepo  = "terraform-aws-eks"
     GithubOrg   = "terraform-aws-modules"
   }
@@ -14,14 +14,14 @@ module "eks" {
 
   worker_groups = [
     {
-      name                          = "rn-worker-group-1"
+      name                          = "my-worker-group-1"
       instance_type                 = "t2.small"
       additional_userdata           = "echo foo bar"
       asg_desired_capacity          = 1
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
     },
     {
-      name                          = "rn-worker-group-2"
+      name                          = "my-worker-group-2"
       instance_type                 = "t2.medium"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
